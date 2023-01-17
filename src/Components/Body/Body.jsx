4dcd77ace9f28a877e1aa5b6e5a5d0d02a2f0bcd
Yhucee14 <uchennachukwu2002@yahoo.com>
2023-01-17 09:46:25 -0800

    pushed

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'; 
-import { Box, Typography, Button , Card, CardMedia} from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import './Body.css';
 
 const Body = () => {
   return (
-    <div>
-        <Grid container>
-            <Grid item md={12}>
-            <Typography variant='h4'>Exclusive beats for the next generation of artists</Typography> 
+    <div >
+        <Grid container className='bodycon'>
+            <Grid item md={12} className='textcon'
+            sx={{ width: "100%",}}>
+              
+            <Typography variant='h3'>Exclusive beats for the </Typography> 
+            <Typography variant='h3'>next generation of</Typography>
+            <Typography variant='h3'> artists</Typography>
             </Grid>
 
-            <Grid item md={12} sx={{ display: { xs: 'block', md: 'flex' } }}>
+            <Grid item md={12} sx={{ display: { xs: 'block', md: 'flex' } }} className='belowcon'>
                 <Box md={4}>
-                <Button id='Navbtn'
-              sx={{ my: 2, color: 'white', display: 'block' }}
+                <Button className='explorebtn' variant='contained'
+              sx={{color: 'white'}}
               component="a"
               href="https://www.google.com/maps/@9.0338725,8.677457,6zcv"
             >
-              Explore
+            Explore
             </Button>
                 </Box>
 
